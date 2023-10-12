0bd53d36d601daae499cadc50f7b0f76477c495f
Eyüp Turgut <eyuptrgtt@gmail.com>
2023-10-12 09:23:23 +0300

    Let's Party !

---
+++ Core.Application/Pipelines/Caching/ICachableRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Application.Pipelines.Caching
+{
+    public interface ICachableRequest
+    {
+        public string CacheKey { get; }
+
+        public bool BypassCache { get; }
+
+        TimeSpan? SlidingExpiration { get; }
+
+
+    }
+}
