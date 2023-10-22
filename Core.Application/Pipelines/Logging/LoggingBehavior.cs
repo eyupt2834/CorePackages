c48ae6afed2daccd1cdc1e4df324a257182987a9
eyupt <eyupt@Ryzen7>
2023-10-22 21:20:35 +0300

    Logging

---
+++ Core.Application/Pipelines/Logging/LoggingBehavior.cs
@@ -0,0 +1,43 @@
+﻿using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.SeriLog;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace Core.Application.Pipelines.Logging
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>, ILoggableRequest
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LoggingBehavior(IHttpContextAccessor httpContextAccessor, LoggerServiceBase logger)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _loggerServiceBase = logger;
+        }
+
+        private readonly LoggerServiceBase _loggerServiceBase;
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            List<LogParameter> logParameters = new List<LogParameter>
+            {
+                 new LogParameter{Type = request.GetType().Name,Value = request }
+            };
+            LogDetail logDetail = new LogDetail
+            {
+                MethodName = next.Method.Name,
+                Parameters = logParameters,
+                User = _httpContextAccessor.HttpContext.User.Identity.Name ?? "?"
+            };
+
+            _loggerServiceBase.Info(JsonSerializer.Serialize(logDetail));
+
+            return await next();
+
+        }
+    }
+}
+
+
+
